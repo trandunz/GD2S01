b7327452fcc315cecc5ab9d8dd53337f7dbde977
trandunz <54924605+trandunz@users.noreply.github.com>
2021-08-02 22:33:45 +1200

    TestScene + Plugins + TestTextures
    Plugins :
    - Volumetric Lights
    - SaberCSG
    
    Textures:
    - Pathing Concrete

---
+++ GD2S01-GAME/Assets/Scripts/Script_UIPB_Test.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_UIPB_Test : MonoBehaviour
+{
+    public Sprite m_sPlayButton;
+    public Sprite m_sPauseButton;
+    public List<Animator> m_aAnimatorList;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GameObject.Find("FloorSpeakers_2").GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<Image>().sprite = m_sPlayButton;
+        }
+    }
+
+    public void Play()
+    {
+        if (!GameObject.Find("FloorSpeakers_2").GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<Image>().sprite = m_sPauseButton;
+            GameObject.Find("FloorSpeakers_2").GetComponent<AudioSource>().Play();
+            foreach (Animator animator in m_aAnimatorList)
+            {
+                if (animator.speed == 0)
+                {
+                    animator.speed = 1;
+                }
+                else
+                {
+                    animator.SetTrigger("Play");
+                }
+                
+            }
+        }
+        else if (GameObject.Find("FloorSpeakers_2").GetComponent<AudioSource>().isPlaying)
+        {
+            GameObject.Find("FloorSpeakers_2").GetComponent<AudioSource>().Pause();
+            foreach (Animator animator in m_aAnimatorList)
+            {
+                animator.speed = 0;
+            }
+        }
+
+
+
+    }
+}
