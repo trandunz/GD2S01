17bb4cb46bd911a8173d26c7ff2865edd3b1476b
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2021-08-04 20:17:44 +1200

    +Room Prefab + Room Move Script
    Made the rooms move by pressing P, currently they stack on top of each other, need to solve that issue.

---
+++ GD2S01-GAME/Assets/Scripts/Josh Scripts/Room_Swap_J.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_Swap_J : MonoBehaviour
+{
+    [SerializeField] Vector3 m_Origin;
+    public Vector3 CurrentPos;
+    public Vector3 PrevPos;
+
+    [SerializeField] int m_iRoomTalley;
+
+    [SerializeField] Room_Swap_J[] m_RoomCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentPos = transform.position;
+        m_Origin = CurrentPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            int itemp = Random.Range(1, m_iRoomTalley);
+            itemp -= 1;
+
+            PrevPos = CurrentPos;
+            CurrentPos = m_RoomCount[itemp].CurrentPos;
+
+            m_RoomCount[itemp].PrevPos = m_RoomCount[itemp].CurrentPos;
+
+            m_RoomCount[itemp].transform.position = m_RoomCount[itemp].CurrentPos;
+
+            m_RoomCount[itemp].CurrentPos = PrevPos;
+        }
+    }
+}
