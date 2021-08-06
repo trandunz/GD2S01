d56f5d6a9a36948627560f46fbfd85dedadc3be5
trandunz <54924605+trandunz@users.noreply.github.com>
2021-08-07 10:03:20 +1200

    + TMPro + Interaction Text + Script_Tool + Scriptable Object for CobWebCleaner

---
+++ GD2S01-GAME/Assets/ScriptableObjects/ScriptableObject_Tool_W.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ToolData", menuName = "ScriptableObjects/Tool")]
+public class ScriptableObject_Tool_W : ScriptableObject
+{
+    public string ToolName;
+
+    public GameObject m_ParticleSystem;
+    public float fInteractRange = 2;
+
+    public AudioClip UseSound;
+}
