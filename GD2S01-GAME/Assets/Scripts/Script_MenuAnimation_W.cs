0c2679a4193105a47dd11fd470258395a2dda318
trandunz <54924605+trandunz@users.noreply.github.com>
2021-08-07 23:52:04 +1200

    +Main Menu + Buttons

---
+++ GD2S01-GAME/Assets/Scripts/Script_MenuAnimation_W.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script_MenuAnimation_W : MonoBehaviour
+{
+    public Button Startb;
+    public Button Continue;
+    public Button Options;
+    public Button Exit;
+    public TMPro.TextMeshProUGUI Title;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayExitAnim()
+    {
+        GetComponent<Animator>().SetTrigger("ExitGame");
+        Startb.gameObject.SetActive(false);
+        Continue.gameObject.SetActive(false);
+        Options.gameObject.SetActive(false);
+        Exit.gameObject.SetActive(false);
+        Title.gameObject.SetActive(false);
+    }
+
+    public void PlayStartAnim()
+    {
+        GetComponent<Animator>().SetTrigger("CameraIn");
+        Startb.gameObject.SetActive(false);
+        Continue.gameObject.SetActive(false);
+        Options.gameObject.SetActive(false);
+        Exit.gameObject.SetActive(false);
+        Title.gameObject.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
+    }
+}
