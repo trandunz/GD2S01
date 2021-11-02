92196f789d5a9cbe77dd630d61315f5b201c8921
JoshB9999 <88113818+JoshB9999@users.noreply.github.com>
2021-11-02 16:24:44 +1300

    room swapping added to the roomswap manager
     only swaps rooms you want to swap

--- GD2S01-GAME/Assets/Scripts/Josh Scripts/Room_Swap_J.cs
+++ GD2S01-GAME/Assets/Scripts/Josh Scripts/Room_Swap_J.cs
@@ -8,6 +8,7 @@ public class Room_Swap_J : MonoBehaviour
     [SerializeField] Room_Variables_J[] m_Rooms;
     // The Number of Rooms in the Array
     [SerializeField] int m_iRoomCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,35 +18,49 @@ public class Room_Swap_J : MonoBehaviour
 
     public void RoomSwap()
     {
-        // Randomize which 2 Rooms will swap
-        // Randomize which 2 Rooms will swap
-        int itemp = Random.Range(1, m_iRoomCount);
-        itemp -= 1;
-        while (m_Rooms[itemp].m_CanMove == false)
+        int iAvailavbletoSwap = 0;
+        for (int i = 0; i < m_iRoomCount; i++)
         {
-            itemp = Random.Range(1, m_iRoomCount);
+            if (m_Rooms[i].m_CanMove)
+            {
+                iAvailavbletoSwap++;
+            }
+            if (iAvailavbletoSwap == 2)
+            {
+                i = m_iRoomCount;
+            }
+        }
+        // Randomize which 2 Rooms will swap
+       if (iAvailavbletoSwap >= 2)
+        {
+            int itemp = Random.Range(1, m_iRoomCount);
             itemp -= 1;
-        }
+            while (m_Rooms[itemp].m_CanMove == false)
+            {
+                itemp = Random.Range(1, m_iRoomCount);
+                itemp -= 1;
+            }
 
-        int itemp2 = Random.Range(1, m_iRoomCount);
-        itemp2 -= 1;
-        while (m_Rooms[itemp2].m_CanMove == false || itemp == itemp2)
-        {
-            itemp2 = Random.Range(1, m_iRoomCount);
+            int itemp2 = Random.Range(1, m_iRoomCount);
             itemp2 -= 1;
+            while (m_Rooms[itemp2].m_CanMove == false || itemp == itemp2)
+            {
+                itemp2 = Random.Range(1, m_iRoomCount);
+                itemp2 -= 1;
+            }
+
+            // Set the rooms which are about to swap to record their current position as their previous position
+            m_Rooms[itemp].PrevPos = m_Rooms[itemp].CurrentPos;
+            m_Rooms[itemp2].PrevPos = m_Rooms[itemp2].CurrentPos;
+
+            // Move the Rooms
+            m_Rooms[itemp].transform.position = m_Rooms[itemp2].PrevPos;
+            m_Rooms[itemp2].transform.position = m_Rooms[itemp].PrevPos;
+
+            // Update the Current Pos Vector in the Rooms
+            m_Rooms[itemp].CurrentPos = m_Rooms[itemp].transform.position;
+            m_Rooms[itemp2].CurrentPos = m_Rooms[itemp2].transform.position;
         }
-
-        // Set the rooms which are about to swap to record their current position as their previous position
-        m_Rooms[itemp].PrevPos = m_Rooms[itemp].CurrentPos;
-        m_Rooms[itemp2].PrevPos = m_Rooms[itemp2].CurrentPos;
-
-        // Move the Rooms
-        m_Rooms[itemp].transform.position = m_Rooms[itemp2].PrevPos;
-        m_Rooms[itemp2].transform.position = m_Rooms[itemp].PrevPos;
-
-        // Update the Current Pos Vector in the Rooms
-        m_Rooms[itemp].CurrentPos = m_Rooms[itemp].transform.position;
-        m_Rooms[itemp2].CurrentPos = m_Rooms[itemp2].transform.position;
     }    
 
     // Update is called once per frame
@@ -54,6 +69,7 @@ public class Room_Swap_J : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             RoomSwap();
+            Debug.Log("Stuff");
         }
     }
 }
