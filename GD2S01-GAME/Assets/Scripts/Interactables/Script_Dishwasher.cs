0a3a25878198899a5308f8e6b72245cd4f4e2ee6
Trandunz <trandunz@gmail.com>
2021-11-07 16:45:02 +1300

    + Sink Is Now Dishwasher

--- GD2S01-GAME/Assets/Scripts/Interactables/Script_Dishwasher.cs
+++ GD2S01-GAME/Assets/Scripts/Interactables/Script_Dishwasher.cs
@@ -6,12 +6,27 @@ public class Script_Dishwasher : MonoBehaviour
 {
     Script_ObjectiveManager_W m_ObjectiveManager;
 
+    [SerializeField] GameObject[] m_Dishes;
+
     void Start()
     {
         m_ObjectiveManager = GameObject.Find("ObjectiveManager").GetComponent<Script_ObjectiveManager_W>();
+        foreach (GameObject dish in m_Dishes)
+        {
+            dish.SetActive(false);
+        }
     }
     public void AddDish()
     {
         m_ObjectiveManager.m_DishNumber--;
+
+        foreach (GameObject dish in m_Dishes)
+        {
+            if (dish.activeSelf == false)
+            {
+                dish.SetActive(true);
+                break;
+            }
+        }
     }
 }
