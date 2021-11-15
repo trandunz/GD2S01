0b4eed4bbb7506302940c1e5f3c80b5073533866
RichW2 <88115307+RichW2@users.noreply.github.com>
2021-11-16 11:49:49 +1300

    Added glow to items when less are in scene

---
+++ GD2S01-GAME/Assets/Scripts/GameManagement/GiveLightWhenFew.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveLightWhenFew : MonoBehaviour
+{
+    public Light lightToSpawn;
+    private bool lightIsSpawned = false;
+    string objsTag;
+    public int numTillSpawn = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objsTag = gameObject.tag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!lightIsSpawned && GameObject.FindGameObjectsWithTag(objsTag).Length <= numTillSpawn)
+        {
+            lightIsSpawned = true;
+            Instantiate(lightToSpawn, transform.position, transform.rotation, transform);
+        }
+    }
+}
